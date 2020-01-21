6ab1d562b90fe91a23dcb21c85b4df191c777ec9
Roy Ge <rgecoder@gmail.com>
2020-01-21 08:56:09 -0800

    Add multiple props

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,21 +2,22 @@ import React from "react"; //first import React library
 import faker from 'faker';
 
 //create a new component in the new file, paste the JSX into it
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+  console.log(props);
   //make a new function that's going to serve
   //as this CommentDetail component
   return (  
     //JSX
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
         <div className="text">Nice Blog Post!</div>
       </div>
--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail author="Sam" timeAgo="Today at 4:55PM" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar={faker.image.avatar()}/>
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatar={faker.image.avatar()}/>
 
-      <CommentDetail />
-      <CommentDetail />
     </div>
   );
 };
