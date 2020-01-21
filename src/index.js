74ffd1b46b98db0dc90c7b90496655ac2ff84802
Roy Ge <rgecoder@gmail.com>
2020-01-21 09:02:18 -0800

    Add format props to components

--- src/index.js
+++ src/index.js
@@ -6,10 +6,21 @@ import CommentDetail from "./CommentDetail";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:55PM" avatar={faker.image.avatar()}/>
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar={faker.image.avatar()}/>
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatar={faker.image.avatar()}/>
-
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:55PM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        avatar={faker.image.avatar()}
+      />
     </div>
   );
 };
