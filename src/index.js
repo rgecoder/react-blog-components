75df4a4a8f586ca2d23065fdb73806cad94fe758
Roy Ge <rgecoder@gmail.com>
2020-01-21 10:43:56 -0800

    Add 40. showing custom childrn

--- src/index.js
+++ src/index.js
@@ -2,25 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard >
       <CommentDetail
         author="Sam"
         timeAgo="Today at 4:55PM"
         avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
+     
+      <ApprovalCard>
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
         avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
+      
+      <ApprovalCard>
       <CommentDetail
         author="Jane"
         timeAgo="Yesterday at 5:00PM"
         avatar={faker.image.avatar()}
       />
+      </ApprovalCard>
     </div>
   );
 };
